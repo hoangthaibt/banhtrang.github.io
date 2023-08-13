9a42106900c73ba1cb4ad7340a25dd81248218ad
hoangthaibt <142157902+hoangthaibt@users.noreply.github.com>
2023-08-13 20:39:41 +0700

    Update bt.js

--- bt.js
+++ bt.js
@@ -3,9 +3,6 @@ function stuff() {
     window.location.href = "stuff.html";
 }
 function addin(){
-    for (let i = 0; i < localStorage.length; ++i){
-        localStorage.setItem(i, localStorage.getItem(i));
-    }
     alert("Sản phẩm đã được chỉnh sửa-thêm vào giỏ hàng");
     var dt = document.getElementsByName("count");
     for (let i = 0; i < dt.length; ++i){
