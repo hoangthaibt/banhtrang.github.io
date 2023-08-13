c5308a2b4db1031a4886ec914b8a33c345ab25c4
hoangthaibt <142157902+hoangthaibt@users.noreply.github.com>
2023-08-13 20:26:33 +0700

    Update bt.js

--- bt.js
+++ bt.js
@@ -3,10 +3,10 @@ function stuff() {
     window.location.href = "stuff.html";
 }
 function addin(){
+    alert("Sản phẩm đã được chỉnh sửa-thêm vào giỏ hàng");
     var dt = document.getElementsByName("count");
     for (let i = 0; i < dt.length; ++i){
         if (Number(dt[i].value) > 0){
-            alert("Sản phẩm đã được chỉnh sửa-thêm vào giỏ hàng");
             localStorage.setItem(i+1, dt[i].value);
         }else if (Number(dt[i].value) == 0){
             localStorage.setItem(i+1, dt[i].value);
